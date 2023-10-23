1cda4f355094bb8423fb86212add658b90e0a993
linei <102635515+linei09@users.noreply.github.com>
2023-10-23 21:54:28 +0700

    Delete php directory

--- php/abcd/controller/Admin-controller/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-   // include constants.php
-   include ('../../model/config/constants.php');
-   //1. destroy  the session 
-   session_destroy();
-    //2. redirect to login page 
-    header('location:'.SITEURL.'controller/Admin-controller/login.php');
-?>
